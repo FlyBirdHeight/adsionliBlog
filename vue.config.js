6208b620e7d3c00e3dea556c2ce10039edd765dc
adsionli <adsionli@foxmail.com>
2021-10-27 10:25:52 +0800

    修改vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -34,4 +34,12 @@ module.exports = {
             }
         }
     },
+    pages: {
+        index: {
+            entry: 'src/main.js',
+            template: 'public/index.html',
+            filename: 'index.html',
+            title: 'adsionliBlog'
+        }
+    }
 };

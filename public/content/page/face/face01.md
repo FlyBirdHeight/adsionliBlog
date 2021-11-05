97eb2da02a899ad2296c612dfdc71e3adda9f11f
adsionli <adsionli@foxmail.com>
2021-11-06 00:04:02 +0800

    代码保存

--- public/content/page/face/face01.md
+++ public/content/page/face/face01.md
@@ -6,3 +6,43 @@
 
 > 面试题就是当我在学习时候，看到有人整理的话，就会进行记录，然后自己实现以下。
 
+## 柯里化相关题目
+
+**第一题：**
+
+```js
+add(1)(2)(3) = 6;
+add(1, 2, 3)(4) = 10;
+add(1)(2)(3)(4)(5) = 15;
+```
+
+**实现上段代码中给出的函数实现**
+
+实现如下：
+
+```js
+var curry = function (fn) {
+    let args = []
+    return function curryHelper (...res) {
+        if (res.length) {
+            args = args.concat(res)
+            return curryHelper
+        } else {
+            return fn.apply(this, args)
+        }
+    }
+}
+
+function add(...res) {
+    return res.reduce((a, b) => a + b)
+}
+
+let addCurry = curry(add);
+console.log(add(1)(2)(3)(4)())
+```
+
+> 解析：这一题就是典型的柯里化的题目，从给出的函数就可以看出来了，那么这里就采用无限制的柯里化实现就可以了，很简单的一道题目
+
+
+
+## 变量相关题目
--- src/views/test/js_test.vue
+++ src/views/test/js_test.vue
@@ -9,23 +9,35 @@
 <script>
 import { throttle, throttleFirstEnd } from '@/funcs/utils/throttle.js'
 function currying(fn, ...rest1) {
-  console.log('currying-out', fn, rest1)
   return function (...rest2) {
-    console.log('currying-in', rest2)
     return fn.apply(null, rest1.concat(rest2))
   }
 }
 function curryingHelper(fn, len) {
-  console.info(fn)
   const length = len || fn.length // 第一遍运行length是函数fn一共需要的参数个数，以后是剩余所需要的参数个数
-  console.log('curryingHelper-out', length, len, fn.length)
   return function (...rest) {
-    console.log('curryingHelper-in', rest, this)
     return rest.length >= length // 检查是否传入了fn所需足够的参数
       ? fn.apply(this, rest)
       : curryingHelper(currying.apply(this, [fn].concat(rest)), length - rest.length) // 在通用currying函数基础上
   }
 }
+
+var curry = function (fn) {
+  let args = []
+  return function curryHelper (...res) {
+    if (res.length) {
+      args = args.concat(res)
+      return curryHelper
+    } else {
+      return fn.apply(this, args)
+    }
+  }
+}
+
+function add(...res) {
+  return res.reduce((a, b) => a + b)
+}
+
 export default {
   data() {
     return {
@@ -34,7 +46,11 @@ export default {
   },
   mounted() {
     // this.handleMouseMove();
-    this.handleCurry()
+    // this.handleCurry()
+    let addCurry = curry(add)
+    // let data = addCurry(1, 2, 3)(2);
+    // console.log(data)
+    console.log(addCurry(1)(2)(3)(4)())
   },
   methods: {
     handleMouseMove() {
